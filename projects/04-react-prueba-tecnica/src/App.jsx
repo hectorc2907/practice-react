e9596f1a3d6d00d47d4cfa64a09146fe68b2b3ca
hectorc2907 <hectorc2907@gmail.com>
2024-06-24 13:39:33 -0300

    se refactoriza para separar en hooks, con esto dejamos funcionando la recuperacion de fact y el manejo del boton get new fact

--- projects/04-react-prueba-tecnica/src/App.jsx
+++ projects/04-react-prueba-tecnica/src/App.jsx
@@ -1,21 +1,22 @@
-import { useEffect, useState } from "react";
-
-const CAT_ENDPOINT_RANDOM_FACT = "https://catfact.ninja/fact";
-const CAT_ENDPOINT_IMAGE_URL = "https://cataas.com/cat/says/hello";
+import "./App.css";
+import { useCatFact } from "./hooks/useCatFact";
 
 export function App() {
-  const [fact, setFact] = useState("lorem ipsum cat fact");
-
-  useEffect(() => {
-    fetch(CAT_ENDPOINT_RANDOM_FACT)
-      .then((res) => res.json())
-      .then((data) => setFact(data.fact));
-  }, []);
-
+  const { fact, refreshFact } = useCatFact();
+  const handleClick = async () => {
+    refreshFact();
+  };
   return (
     <main>
       <h1>App de Gatitos</h1>
+      <button onClick={handleClick}>Get new fact</button>
       {fact && <p>{fact}</p>}
+      {/* {imageUrl && (
+        <img
+          src={imageUrl}
+          alt={`Image extracted using the first three words for ${fact}`}
+        />
+      )} */}
     </main>
   );
 }
