fd1b8d6e3e2b800b7e2604082420a16401b09b5e
hectorc2907 <hectorc2907@gmail.com>
2024-06-23 22:32:26 -0300

    se dibujan los espacios del tablero que se utilizara para cada jugador

--- projects/02-tic-tac-toe/src/App.jsx
+++ projects/02-tic-tac-toe/src/App.jsx
@@ -1,11 +1,25 @@
-import './App.css'
+import "./App.css";
+
+const TURN = {
+  X: "x",
+  O: "o",
+};
+
+const board = Array(9).fill(null);
 
 export function App() {
-
-
   return (
-    <>
-      
-    </>
-  )
+    <main className="board">
+      <h1>Tic Tac Toe</h1>
+      <section className="game">
+        {board.map((_, index) => {
+          return (
+            <div className="cell" key={index}>
+              <span className="cell__content">{index}</span>
+            </div>
+          );
+        })}
+      </section>
+    </main>
+  );
 }
