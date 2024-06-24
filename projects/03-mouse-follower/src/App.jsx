7d6686f97c813fd5427f1c6b91ae23a704dcc693
hectorc2907 <hectorc2907@gmail.com>
2024-06-24 02:08:32 -0300

    se realiza la configuracion de la logica para generar el circulo que seguira al puntero junto con el boton y la logica para activarlo

--- projects/03-mouse-follower/src/App.jsx
+++ projects/03-mouse-follower/src/App.jsx
@@ -1,5 +1,46 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
+const FollowMouse = () => {
+  const [enabled, setEnabled] = useState(false);
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+  useEffect(() => {
+    console.log("effect", { enabled });
+    const handleMove = (event) => {
+      const { clientX, clientY } = event;
+      setPosition({ x: clientX, y: clientY });
+    };
+    if (enabled) {
+      window.addEventListener("pointermove", handleMove);
+    }
+  });
+  return (
+    <>
+      <div
+        style={{
+          position: "absolute",
+          backgroundColor: "rgba(0, 0, 0, 0.5)",
+          border: "1px solid #fff",
+          borderRadius: "50%",
+          opacity: 0.8,
+          pointerEvents: "none",
+          left: -25,
+          top: -25,
+          width: 50,
+          height: 50,
+          transform: `translate(${position.x}px,${position.y}px)`,
+        }}
+      />
+      <button onClick={() => setEnabled(!enabled)}>
+        {enabled ? "Desactivar" : "Activar"} Seguir Puntero
+      </button>
+    </>
+  );
+};
 export function App() {
-  return <div>MouseFollower</div>;
+  return (
+    <main>
+      <FollowMouse />
+    </main>
+  );
 }
