ee01de5451d2ae3b0230985140d7ba3e1e66bcef
hectorc2907 <hectorc2907@gmail.com>
2024-06-24 16:23:23 -0300

    se realiza una muestra de los datos principales que se piden extraidos del mocks

--- projects/05-react-buscador-peliculas/src/App.jsx
+++ projects/05-react-buscador-peliculas/src/App.jsx
@@ -1,6 +1,10 @@
 import "./App.css";
+import responseMovies from "./mocks/with-results.json";
+import withoutResult from "./mocks/no-result.json";
 
 export function App() {
+  const movies = responseMovies.Search;
+  const hasMovies = movies?.length > 0;
   return (
     <div className="page">
       <header>
@@ -10,7 +14,21 @@ export function App() {
           <button type="submit">Buscar</button>
         </form>
       </header>
-      <main></main>
+      <main>
+        {hasMovies ? (
+          <ul>
+            {movies.map((movie) => (
+              <li key={movie.imdbID}>
+                <h3>{movie.Title}</h3>
+                <p>{movie.Year}</p>
+                <img src={movie.Poster} alt={movie.Title} />
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <p>No se encontraron resultados</p>
+        )}
+      </main>
     </div>
   );
 }
