64334ebac9747114bca3da555b3bbcdf14dc1eec
hectorc2907 <hectorc2907@gmail.com>
2024-06-24 15:50:03 -0300

    se limpia la base del proyecto y se copian los estilos a utilizar en el ejercicio

---
+++ projects/05-react-buscador-peliculas/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+export function App() {
+  return (
+    <div>
+      <form className="form">
+        <input placeholder="Avenger, Star Wars, The Matrix..." />
+        <button type="submit">Buscar</button>
+      </form>
+    </div>
+  );
+}
