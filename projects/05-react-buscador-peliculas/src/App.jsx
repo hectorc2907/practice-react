bb5a3c916df5208deacf9de254e80b457419b7a9
hectorc2907 <hectorc2907@gmail.com>
2024-06-24 16:09:10 -0300

    se crea la carpeta mocks para copiar los datos resultantes y no gastar el uso de la api

--- projects/05-react-buscador-peliculas/src/App.jsx
+++ projects/05-react-buscador-peliculas/src/App.jsx
@@ -2,11 +2,15 @@ import "./App.css";
 
 export function App() {
   return (
-    <div>
-      <form className="form">
-        <input placeholder="Avenger, Star Wars, The Matrix..." />
-        <button type="submit">Buscar</button>
-      </form>
+    <div className="page">
+      <header>
+        <h1>Buscador de Peliculas</h1>
+        <form className="form">
+          <input placeholder="Avenger, Star Wars, The Matrix..." />
+          <button type="submit">Buscar</button>
+        </form>
+      </header>
+      <main></main>
     </div>
   );
 }
