cc7c0917f99ee2162361678c7483ce34985152f5
hectorc2907 <hectorc2907@gmail.com>
2024-06-25 15:49:45 -0300

    se trabaja el Cart para poder agregar productos a un carrito y poder eliminarlos, usando un context, hook, y un reducers con la logica para guardarlo en el localStorage, removerlo o actualizarlo, se carga este context en el app.jsx y tambien en la parte de products.jsx

--- projects/06-shopping-cart/src/App.jsx
+++ projects/06-shopping-cart/src/App.jsx
@@ -1,21 +1,22 @@
 import { products as initialProducts } from "./mocks/products.json";
 import { Products } from "./components/Products";
-import { useState } from "react";
 import { Header } from "./components/Header";
 import { Footer } from "./components/Footers";
 import { IS_DEVELOPMENT } from "./config";
 import { useFilters } from "./hooks/useFilters";
+import { Cart } from "./components/Cart";
+import { CartProvider } from "./context/cart";
 
 function App() {
-  const [products] = useState(initialProducts);
   const { filterProducts } = useFilters();
-  const filteredProducts = filterProducts(products);
+  const filteredProducts = filterProducts(initialProducts);
   return (
-    <>
+    <CartProvider>
       <Header />
+      <Cart />
       <Products products={filteredProducts} />
       {IS_DEVELOPMENT && <Footer />}
-    </>
+    </CartProvider>
   );
 }
 
