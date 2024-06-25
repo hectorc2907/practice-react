24694f68bf172aa62d88245c9960580b2b2cc7dc
hectorc2907 <hectorc2907@gmail.com>
2024-06-25 13:26:25 -0300

    se implementa el uso de useId con la finalidad de tener identificadores unicos en los filtros

--- projects/06-shopping-cart/src/components/Filter.jsx
+++ projects/06-shopping-cart/src/components/Filter.jsx
@@ -1,8 +1,10 @@
-import { useState } from "react";
+import { useState, useId } from "react";
 import "./Filter.css";
 
 export function Filter({ changeFilters }) {
   const [minPrice, setMinPrice] = useState();
+  const minPriceFilterId = useId();
+  const categoryFilterId = useId();
 
   const handleChangeMinPrice = (event) => {
     setMinPrice(event.target.value);
@@ -21,10 +23,10 @@ export function Filter({ changeFilters }) {
   return (
     <section className="filters">
       <div>
-        <label htmlFor="price">Price</label>
+        <label htmlFor={minPriceFilterId}>Price</label>
         <input
           type="range"
-          id="price"
+          id={minPriceFilterId}
           min="0"
           max="1000"
           onChange={handleChangeMinPrice}
@@ -32,8 +34,8 @@ export function Filter({ changeFilters }) {
         <span>${minPrice}</span>
       </div>
       <div>
-        <label htmlFor="category">Categoria</label>
-        <select id="category" onChange={handleChangeCategory}>
+        <label htmlFor={categoryFilterId}>Categoria</label>
+        <select id={categoryFilterId} onChange={handleChangeCategory}>
           <option value="all">Todas</option>
           <option value="laptops">Portatiles</option>
           <option value="smartphones">Celulares</option>
