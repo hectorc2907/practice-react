2d37fad1822026f8cadce14498f9105b5851b384
hectorc2907 <hectorc2907@gmail.com>
2024-06-24 23:52:35 -0300

    se realiza la logica para filtrar por orden alfabetico usando useMemo

--- projects/05-react-buscador-peliculas/src/App.jsx
+++ projects/05-react-buscador-peliculas/src/App.jsx
@@ -36,12 +36,17 @@ function useSearch() {
 }
 
 export function App() {
+  const [sort, setSort] = useState(false);
   const { search, updateSearch, error } = useSearch();
-  const { movies, getMovies, loading } = useMovies({ search });
+  const { movies, getMovies, loading } = useMovies({ search, sort });
 
   const handleSubmit = (event) => {
     event.preventDefault();
     getMovies();
+  };
+
+  const handleSort = () => {
+    setSort(!sort);
   };
 
   const handleChange = (event) => {
@@ -63,6 +68,7 @@ export function App() {
             name="query"
             placeholder="Avenger, Star Wars, The Matrix..."
           />
+          <input type="checkbox" onChange={handleSort} checked={sort} />
           <button type="submit">Buscar</button>
         </form>
         {error && <p style={{ color: "red" }}>{error}</p>}
--- projects/05-react-buscador-peliculas/src/hooks/useMovies.js
+++ projects/05-react-buscador-peliculas/src/hooks/useMovies.js
@@ -1,7 +1,7 @@
-import { useState, useRef } from "react";
+import { useState, useRef, useMemo } from "react";
 import { searchMovies } from "../services/movies";
 
-export function useMovies({ search }) {
+export function useMovies({ search, sort }) {
   const [movies, setMovies] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
@@ -20,5 +20,11 @@ export function useMovies({ search }) {
       setLoading(false);
     }
   };
-  return { movies, getMovies, loading };
+  const sortedMovies = useMemo(() => {
+    if (!movies) return;
+    return sort
+      ? [...movies].sort((a, b) => a.title.localeCompare(b.title))
+      : movies;
+  }, [sort, movies]);
+  return { movies: sortedMovies, getMovies, loading };
 }
