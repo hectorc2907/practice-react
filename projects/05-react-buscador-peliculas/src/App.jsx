da36082f89d5373656daa45e87759a65ab1f744e
hectorc2907 <hectorc2907@gmail.com>
2024-06-24 22:30:01 -0300

    se agrega la logica de busqueda para el manejo de errores

--- projects/05-react-buscador-peliculas/src/App.jsx
+++ projects/05-react-buscador-peliculas/src/App.jsx
@@ -1,14 +1,51 @@
 import "./App.css";
 import { useMovies } from "./hooks/useMovies";
 import { Movies } from "./components/Movies";
+import { useEffect, useState, useRef } from "react";
+
+function useSearch() {
+  const [search, updateSearch] = useState("");
+  const [error, setError] = useState(null);
+  const isFirstInput = useRef(true);
+
+  useEffect(() => {
+    if (isFirstInput.current) {
+      isFirstInput.current = search === "";
+      return;
+    }
+
+    if (search === "") {
+      setError("No se puede buscar una pelicula vacia");
+      return
+    }
+
+    if (search.match(/^\d+$/)) {
+      setError("No se puede buscar una pelicula con un numero");
+      return
+    }
+
+    if (search.length < 3) {
+      setError("La busqueda debe tener al menos 3 caracteres");
+      return
+    }
+
+    setError(null);
+  }, [search]);
+
+  return { search, updateSearch, error };
+}
 
 export function App() {
   const { movies } = useMovies();
+  const { search, updateSearch, error } = useSearch();
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    const inputEl = inputRef.current;
-    const value = inputEl.value;
+    console.log({ search });
+  };
+
+  const handleChange = (event) => {
+    updateSearch(event.target.value);
   };
 
   return (
@@ -16,9 +53,19 @@ export function App() {
       <header>
         <h1>Buscador de Peliculas</h1>
         <form className="form" onSubmit={handleSubmit}>
-          <input placeholder="Avenger, Star Wars, The Matrix..." />
+          <input
+            style={{
+              border: "1px solid transparent",
+              borderColor: error ? "red" : "transparent",
+            }}
+            onChange={handleChange}
+            value={search}
+            name="query"
+            placeholder="Avenger, Star Wars, The Matrix..."
+          />
           <button type="submit">Buscar</button>
         </form>
+        {error && <p style={{ color: "red" }}>{error}</p>}
       </header>
       <main>
         <Movies movies={movies} />
