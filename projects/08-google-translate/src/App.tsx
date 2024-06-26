6259f4aac33bfc00db67951bfc46743da268db35
Hector Clessi <121068920+hectorc2907@users.noreply.github.com>
2024-06-26 19:58:46 -0300

    se realiza el tipado de datos en un archivo independiente llamado type.d, ademas se crea un custom hook llamado useStore para poder organizar mejor el uso de los archivos, la finalidad es que las acciones esten definidas pero no directamente expuestas

--- projects/08-google-translate/src/App.tsx
+++ projects/08-google-translate/src/App.tsx
@@ -1,55 +1,18 @@
-import { useReducer } from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-const initialState = {
-  fromLanguage: "auto",
-  toLanguage: "en",
-  fromText: "",
-  result: "",
-  loading: false,
-};
-
-function reducer(state, action) {
-  const { type, payload } = action;
-  if (type === "INTERCHANGE_LANGUAGES") {
-    return {
-      ...state,
-      fromLanguage: state.toLanguage,
-      toLanguage: state.fromLanguage,
-    };
-  }
-
-  if (type === "SET_FROM_LANGUAGE") {
-    return {
-      ...state,
-      fromLanguage: payload,
-    };
-  }
-
-  if (type === "SET_TO_LANGUAGE") {
-    return {
-      ...state,
-      toLanguage: payload,
-    };
-  }
-
-  if (type === "SET_FROM_TEXT") {
-    return { ...state, loading: true, fromText: payload, result: "" };
-  }
-
-  if (type === "SET_RESULT") {
-    return { ...state, loading: false, result: payload };
-  }
-
-  return state;
-}
-
 function App() {
-  const [state, dispatch] = useReducer(reducer, initialState);
+  
   return (
     <div className="App">
       <h1>Google Translate</h1>
+      <button
+        onClick={() => {
+          dispatch({ type: "SET_FROM_LANGUAGE", payload: "es" });
+        }}
+      >
+        Cambiar a Español
+      </button>
     </div>
   );
 }
