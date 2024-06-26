8400da15383b2c7887046f6418ffaf1e903be7e4
hectorc2907 <hectorc2907@gmail.com>
2024-06-26 10:42:40 -0300

    se genera un renderizado condicional para probar la logica del React Router

--- projects/07-midu-router/src/App.jsx
+++ projects/07-midu-router/src/App.jsx
@@ -1,9 +1,39 @@
+import { useState } from "react";
 import "./App.css";
 
+function HomePage() {
+  return (
+    <div>
+      <h1>Home</h1>
+      <p>Esta es una pagina de ejemplo para crear un React Router</p>
+      <a href="/about">Sobre Nosotros</a>
+    </div>
+  );
+}
+
+function AboutPage() {
+  return (
+    <div>
+      <h1>About</h1>
+      <div>
+        <img
+          src="https://pbs.twimg.com/profile_images/1805291672273575936/IlcQEzur_400x400.jpg"
+          alt="mi foto"
+        />
+        <p>Hola!, estoy creando un React Router desde cero</p>
+      </div>
+      <a href="/">Home</a>
+    </div>
+  );
+}
+
 function App() {
+  const [currentPath, setCurrentPath] = useState(window.location.pathname);
   return (
     <main>
       <div>Midu Router</div>
+      {currentPath === "/" && <HomePage />}
+      {currentPath === "/about" && <AboutPage />}
     </main>
   );
 }
