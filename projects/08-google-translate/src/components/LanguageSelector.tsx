6578490207152f1e6f41e2a7007b8d776eb3b2e6
Hector Clessi <121068920+hectorc2907@users.noreply.github.com>
2024-06-27 00:05:06 -0300

    se realiza la reorganizacion de los archivos y se crea el hook useDebounce

--- projects/08-google-translate/src/components/LanguageSelector.tsx
+++ projects/08-google-translate/src/components/LanguageSelector.tsx
@@ -1,6 +1,6 @@
 import { Form } from "react-bootstrap";
 import { AUTO_LANGUAGE, SUPPORTED_LANGUAGES } from "../constant";
-import { FromLanguage, SectionType, type Language } from "../types.d";
+import { SectionType, type FromLanguage, type Language } from "../types.d";
 
 type Props =
   | {
@@ -8,11 +8,17 @@ type Props =
       value: FromLanguage;
       onChange: (language: FromLanguage) => void;
     }
-  | { type: SectionType.To; value: Language; onChange: (language: Language) => void };
+  | {
+      type: SectionType.To;
+      value: Language;
+      onChange: (language: Language) => void;
+    };
+
 export const LanguageSelector = ({ onChange, type, value }: Props) => {
   const handleChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
     onChange(event.target.value as Language);
   };
+
   return (
     <Form.Select
       aria-label="Selecciona el idioma"
@@ -20,8 +26,9 @@ export const LanguageSelector = ({ onChange, type, value }: Props) => {
       value={value}
     >
       {type === SectionType.From && (
-        <option value={AUTO_LANGUAGE}>Detectar Idioma</option>
+        <option value={AUTO_LANGUAGE}>Detectar idioma</option>
       )}
+
       {Object.entries(SUPPORTED_LANGUAGES).map(([key, literal]) => (
         <option key={key} value={key}>
           {literal}
--- projects/08-google-translate/src/hooks/useStore.ts
+++ projects/08-google-translate/src/hooks/useStore.ts
@@ -1,7 +1,13 @@
 import { useReducer } from "react";
-import { Action, Language, State, FromLanguage } from "../types.d";
 import { AUTO_LANGUAGE } from "../constant";
+import {
+  type FromLanguage,
+  type Language,
+  type Action,
+  type State,
+} from "../types";
 
+// 1. Create a initialState
 const initialState: State = {
   fromLanguage: "auto",
   toLanguage: "en",
@@ -10,43 +16,75 @@ const initialState: State = {
   loading: false,
 };
 
+// 2. Create a reducer
 function reducer(state: State, action: Action) {
   const { type } = action;
+
   if (type === "INTERCHANGE_LANGUAGES") {
+    // lógica del estado dentro del reducer
+    // porque lo evitamos en los componentes
     if (state.fromLanguage === AUTO_LANGUAGE) return state;
+
+    const loading = state.fromText !== "";
+
     return {
       ...state,
+      loading,
+      result: "",
       fromLanguage: state.toLanguage,
       toLanguage: state.fromLanguage,
     };
   }
 
   if (type === "SET_FROM_LANGUAGE") {
+    if (state.fromLanguage === action.payload) return state;
+
+    const loading = state.fromText !== "";
+
     return {
       ...state,
       fromLanguage: action.payload,
+      result: "",
+      loading,
     };
   }
 
   if (type === "SET_TO_LANGUAGE") {
+    if (state.toLanguage === action.payload) return state;
+    const loading = state.fromText !== "";
+
     return {
       ...state,
       toLanguage: action.payload,
+      result: "",
+      loading,
     };
   }
 
   if (type === "SET_FROM_TEXT") {
-    return { ...state, loading: true, fromText: action.payload, result: "" };
+    const loading = action.payload !== "";
+
+    return {
+      ...state,
+      loading,
+      fromText: action.payload,
+      result: "",
+    };
   }
 
   if (type === "SET_RESULT") {
-    return { ...state, loading: false, result: action.payload };
+    return {
+      ...state,
+      loading: false,
+      result: action.payload,
+    };
   }
 
   return state;
 }
 
 export function useStore() {
+  // 3. usar el hook useReducer
   const [{ fromLanguage, toLanguage, fromText, result, loading }, dispatch] =
     useReducer(reducer, initialState);
 
