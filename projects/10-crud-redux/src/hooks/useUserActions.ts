d9647c546c4fd593c5e6d97832b2cacc19251def
Hector Clessi <121068920+hectorc2907@users.noreply.github.com>
2024-06-27 15:29:40 -0300

    creamos el hook para las acciones del usuario y lo configuramos con el delete

---
+++ projects/10-crud-redux/src/hooks/useUserActions.ts
@@ -0,0 +1,13 @@
+import { UserId, deleteUserById } from "../store/users/slice";
+import { useAppDispatch } from "./store";
+
+export const useUserActions = () => {
+  const dispatch = useAppDispatch();
+
+  const removeUser = (id: UserId) => {
+    dispatch(deleteUserById(id));
+  };
+  return {
+    removeUser,
+  };
+};
