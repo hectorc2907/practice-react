116a2e3481b8c90c867f1f191bd395d762b2f49c
Hector Clessi <121068920+hectorc2907@users.noreply.github.com>
2024-06-27 02:23:54 -0300

    se configura el archivo main.tsx con la finalidad de mostrar los elementos sin el stric.mode, se crea el componente Header.tsx y se lo implemente en al App.tsx, ademas de ser configurado

--- projects/08-todo-app-typescript/src/App.tsx
+++ projects/08-todo-app-typescript/src/App.tsx
@@ -1,5 +1,15 @@
-function App() {
-  return <div>TODO</div>;
-}
+import { Header } from "./components/Header";
+import { useTodos } from "./hooks/useTodoFirst";
+
+const App: React.FC = () => {
+  const { handleSave } = useTodos();
+  return (
+    <>
+      <div className="todoapp">
+        <Header saveTodo={handleSave} />
+      </div>
+    </>
+  );
+};
 
 export default App;
