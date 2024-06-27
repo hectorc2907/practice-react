eef2a02ad372d2e9007589fdde2c755be5adba66
Hector Clessi <121068920+hectorc2907@users.noreply.github.com>
2024-06-27 15:08:56 -0300

    creamos el hook.ts con la finalidad de tipar los selectores y evitar problemas con typescript y redux

--- projects/10-crud-redux/src/store/index.ts
+++ projects/10-crud-redux/src/store/index.ts
@@ -4,3 +4,6 @@ import usersReducer from "./users/slice";
 export const store = configureStore({
   reducer: { users: usersReducer },
 });
+
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
