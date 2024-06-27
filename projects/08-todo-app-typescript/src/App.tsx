5a397949084d7c78a01e98b0aeeb63241f5f63df
Hector Clessi <121068920+hectorc2907@users.noreply.github.com>
2024-06-27 03:28:38 -0300

    se agrega el componente de Todos al App.tsx para mostrar los todos guardados en el mocks

--- projects/08-todo-app-typescript/src/App.tsx
+++ projects/08-todo-app-typescript/src/App.tsx
@@ -1,12 +1,25 @@
 import { Header } from "./components/Header";
+import { Todos } from "./components/Todos";
 import { useTodos } from "./hooks/useTodoFirst";
 
 const App: React.FC = () => {
-  const { handleSave } = useTodos();
+  const {
+    handleCompleted,
+    handleSave,
+    handleRemove,
+    handleUpdateTitle,
+    todos: filteredTodos,
+  } = useTodos();
   return (
     <>
       <div className="todoapp">
         <Header saveTodo={handleSave} />
+        <Todos
+          removeTodo={handleRemove}
+          setCompleted={handleCompleted}
+          setTitle={handleUpdateTitle}
+          todos={filteredTodos}
+        />
       </div>
     </>
   );
