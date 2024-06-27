0b1012b8a2f60e8347b0885dea7232d99228f98c
Hector Clessi <121068920+hectorc2907@users.noreply.github.com>
2024-06-27 16:40:13 -0300

    se realiza un manejo de errores al momento de realizar el createNewUser

--- projects/10-crud-redux/src/components/CreateNewUser.tsx
+++ projects/10-crud-redux/src/components/CreateNewUser.tsx
@@ -1,10 +1,16 @@
-import { Button, Card, TextInput, Title } from "@tremor/react";
+import { Badge, Button, Card, TextInput, Title } from "@tremor/react";
+import { useState } from "react";
 import { useUserActions } from "../hooks/useUserActions";
 
 export function CreateNewUser() {
   const { addUser } = useUserActions();
+  const [result, setResult] = useState<"ok" | "ko" | null>(null);
+
   const handleSubmit = (event: React.FormEvent<HTMLFormEvent>) => {
     event.preventDefault();
+
+    setResult(null);
+
     const form = event.target;
     const formData = new FormData(form);
 
@@ -12,7 +18,13 @@ export function CreateNewUser() {
     const email = formData.get("email") as string;
     const github = formData.get("github") as string;
 
+    if (!name || !email || !github) {
+      return setResult("ko");
+    }
+
     addUser({ name, email, github });
+    setResult("ok");
+    form.reset();
   };
   return (
     <Card style={{ marginTop: "16px" }}>
@@ -25,6 +37,12 @@ export function CreateNewUser() {
           <Button type="submit" style={{ marginTop: "16px" }}>
             Crear Usuario
           </Button>
+          <span>
+            {result === "ok" && (
+              <Badge color="green">Guardado correctamente</Badge>
+            )}
+            {result === "ko" && <Badge color="red">Error con los campos</Badge>}
+          </span>
         </div>
       </form>
     </Card>
