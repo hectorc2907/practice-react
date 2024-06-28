b1c5ef53505dc897d364f9abf34f4fcf3cb9b801
Hector Clessi <121068920+hectorc2907@users.noreply.github.com>
2024-06-27 23:35:28 -0300

    se genera la funcion para ordenar alfabeticamente por pais

--- projects/11-typescript-prueba-tecnica/src/App.tsx
+++ projects/11-typescript-prueba-tecnica/src/App.tsx
@@ -1,14 +1,21 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import { UsersList } from "./components/UsersList";
+import { type User } from "./type";
 
 function App() {
-  const [users, setUsers] = useState([]);
+  const [users, setUsers] = useState<User[]>([]);
   const [showColors, setShowColors] = useState(false);
+  const [sortByCountry, setSortByCountry] = useState(false);
 
   const toggleColors = () => {
     setShowColors(!showColors);
   };
+
+  const toggleSortByCountry = () => {
+    setSortByCountry((prevState) => !prevState);
+  };
+
   useEffect(() => {
     fetch(`https://randomuser.me/api?results=100`)
       .then((res) => res.json())
@@ -20,14 +27,23 @@ function App() {
       });
   }, []);
 
+  const sortedUsers = sortByCountry
+    ? users.toSorted((a, b) => {
+        return a.location.country.localeCompare(b.location.country);
+      })
+    : users;
+
   return (
     <>
       <h1>Lista de usuarios</h1>
       <header>
         <button onClick={toggleColors}>Colorear filas</button>
+        <button onClick={toggleSortByCountry}>
+          {sortByCountry ? "No ordenar por pais" : "Ordenar por pais"}
+        </button>
       </header>
       <main>
-        <UsersList showColors={showColors} users={users} />
+        <UsersList showColors={showColors} users={sortedUsers} />
       </main>
     </>
   );
