72bc7234a82c5c544129f7f3e2b0456aaf14eba2
Hector Clessi <121068920+hectorc2907@users.noreply.github.com>
2024-06-28 02:47:43 -0300

    se agregan los archivos faltantes y se reparan bugs, queda pendiente el resetear estado

--- projects/11b-typescript-prueba-tecnica-with-react-query/src/hooks/useUser.ts
+++ projects/11b-typescript-prueba-tecnica-with-react-query/src/hooks/useUser.ts
@@ -1,24 +1,45 @@
 import { fetchUsers } from "../services/users";
 import { useInfiniteQuery } from "@tanstack/react-query";
+import { useState, useEffect } from "react";
 import { type User } from "../types.d";
 
 export const useUsers = () => {
-  const { isLoading, isError, data, refetch, fetchNextPage, hasNextPage } =
-    useInfiniteQuery<{ nextCursor?: number; users: User[] }>(
-      ["users"],
-      fetchUsers,
-      {
-        getNextPageParam: (lastPage) => lastPage.nextCursor,
-        refetchOnWindowFocus: false,
-        staleTime: 1000 * 3,
-      }
-    );
+  const [users, setUsers] = useState<User[]>([]);
+
+  const {
+    isLoading,
+    isError,
+    data,
+    refetch,
+    fetchNextPage,
+    hasNextPage,
+  } = useInfiniteQuery({
+    queryKey: ["users"],
+    queryFn: ({ pageParam = 0 }) => fetchUsers({pageParam}),
+    getNextPageParam: (lastPage) => lastPage.nextCursor,
+    refetchOnWindowFocus: false,
+    staleTime: 1000 * 3,
+    initialPageParam: 0,
+  });
+
+  useEffect(() => {
+    if (data) {
+      const allUsers = data.pages.flatMap((page) => page.users);
+      setUsers(allUsers);
+    }
+  }, [data]);
+
+  const deleteUser = (email: string) => {
+    setUsers((prevUsers) => prevUsers.filter((user) => user.email !== email));
+  };
+
   return {
     refetch,
     fetchNextPage,
     isLoading,
     isError,
-    users: data?.pages.flatMap((page) => page.users) ?? [],
+    users,
     hasNextPage,
+    deleteUser,
   };
 };
