a7365960cc9ae31205a7ee82a5b36a1d0e931c3f
Hector Clessi <121068920+hectorc2907@users.noreply.github.com>
2024-06-27 22:09:38 -0300

    se realiza la recuperacion de los datos de la api, en especifico los 100 usuarios que solicita la prueba

--- projects/11-typescript-prueba-tecnica/src/App.tsx
+++ projects/11-typescript-prueba-tecnica/src/App.tsx
@@ -1,12 +1,25 @@
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch(`https://randomuser.me/api?rsults=100`)
+      .then((res) => res.json())
+      .then((res) => {
+        setUsers(res.results);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }, []);
 
   return (
     <>
       <h1>Lista de usuarios</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
