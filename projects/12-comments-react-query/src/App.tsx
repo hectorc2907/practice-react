8a9f6a825f4be08b881fc0706be3e30c356c8e94
Hector Clessi <121068920+hectorc2907@users.noreply.github.com>
2024-06-28 14:12:36 -0300

    se ingresan los componentes del form en app.tsx y se configuran algunos estilos

--- projects/12-comments-react-query/src/App.tsx
+++ projects/12-comments-react-query/src/App.tsx
@@ -1,10 +1,25 @@
 import "./App.css";
+import { FormInput, FormTextArea } from "./components/Form";
 
 function App() {
   return (
-    <>
-      <h1>Comments</h1>
-    </>
+    <main className="grid h-screen grid-cols-2">
+      <div className="col-span-1 p-8 bg-white">
+        <h1>Comments</h1>
+      </div>
+      <div className="col-span-1 p-8 bg-black">
+        <form className="block max-w-xl px-4 m-auto">
+          <FormInput />
+          <FormTextArea />
+          <button
+            type="submit"
+            className="mt-4 px-12 text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm py-2.5 text-center mr-2 mb-2"
+          >
+            Enviar Comentario
+          </button>
+        </form>
+      </div>
+    </main>
   );
 }
 
