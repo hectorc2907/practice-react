82e246ff48e1f99de29b115a7e536e8e30977527
Hector Clessi <121068920+hectorc2907@users.noreply.github.com>
2024-06-28 23:06:05 -0300

    se realiza la muestra de las opciones para responder del juego

--- projects/13-javascript-quiz-con-zustand/src/Game.tsx
+++ projects/13-javascript-quiz-con-zustand/src/Game.tsx
@@ -1,4 +1,13 @@
-import { Card, IconButton, Stack, Typography } from "@mui/material";
+import {
+  Card,
+  IconButton,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+  Stack,
+  Typography,
+} from "@mui/material";
 import { useQuestionStore } from "./store/questions";
 import SyntaxHighLighter from "react-syntax-highlighter";
 import { gradientDark } from "react-syntax-highlighter/dist/esm/styles/hljs";
@@ -6,11 +15,20 @@ import { type Question as QuestionType } from "./types";
 
 const Question = ({ info }: { info: QuestionType }) => {
   return (
-    <Card variant="outlined" sx={{ p: 2, textAlign: "left" }}>
+    <Card variant="outlined" sx={{ bgcolor: "#222", p: 2, textAlign: "left" }}>
       <Typography variant="h5">{info.question}</Typography>
       <SyntaxHighLighter language="javascript" style={gradientDark}>
         {info.code}
       </SyntaxHighLighter>
+      <List sx={{ bgcolor: "#333" }} disablePadding>
+        {info.answers.map((answer, index) => (
+          <ListItem key={index} disablePadding divider>
+            <ListItemButton>
+              <ListItemText primary={answer} sx={{ textAlign: "center" }} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
     </Card>
   );
 };
