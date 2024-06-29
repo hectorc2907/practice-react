d9ff4c8319cabf47129a763b4b9aeaaee454d4a5
Hector Clessi <121068920+hectorc2907@users.noreply.github.com>
2024-06-29 00:41:43 -0300

    agregamos persist en el store para poder conservar los cambios al recargar

--- projects/13-javascript-quiz-con-zustand/src/store/questions.ts
+++ projects/13-javascript-quiz-con-zustand/src/store/questions.ts
@@ -1,6 +1,7 @@
 import { create } from "zustand";
 import { type Question } from "../types";
 import confetti from "canvas-confetti";
+import { persist } from "zustand/middleware";
 
 interface State {
   questions: Question[];
@@ -11,46 +12,58 @@ interface State {
   goPreviousQuestion: () => void;
 }
 
-export const useQuestionStore = create<State>((set, get) => {
-  return {
-    questions: [],
-    currentQuestion: 0,
-    fetchQuestions: async (limit: number) => {
-      const res = await fetch("http://localhost:5173/data.json");
-      const json = await res.json();
+export const useQuestionStore = create<State>()(
+  persist(
+    (set, get) => {
+      return {
+        questions: [],
+        currentQuestion: 0,
+        fetchQuestions: async (limit: number) => {
+          const res = await fetch("http://localhost:5173/data.json");
+          const json = await res.json();
 
-      const questions = json.sort(() => Math.random() - 0.5).slice(0, limit);
-      set({ questions });
-    },
-    selectAnswer: (questionId: number, answerIndex: number) => {
-      const { questions } = get();
-      const newQuestions = structuredClone(questions);
-      const questionIndex = newQuestions.findIndex((q) => q.id === questionId);
-      const questionInfo = newQuestions[questionIndex];
-      const isCorrectUserAnswer = questionInfo.correctAnswer === answerIndex;
-      if (isCorrectUserAnswer) confetti();
-      newQuestions[questionIndex] = {
-        ...questionInfo,
-        isCorrectUserAnswer,
-        userSelectedAnswer: answerIndex,
+          const questions = json
+            .sort(() => Math.random() - 0.5)
+            .slice(0, limit);
+          set({ questions });
+        },
+        selectAnswer: (questionId: number, answerIndex: number) => {
+          const { questions } = get();
+          const newQuestions = structuredClone(questions);
+          const questionIndex = newQuestions.findIndex(
+            (q) => q.id === questionId
+          );
+          const questionInfo = newQuestions[questionIndex];
+          const isCorrectUserAnswer =
+            questionInfo.correctAnswer === answerIndex;
+          if (isCorrectUserAnswer) confetti();
+          newQuestions[questionIndex] = {
+            ...questionInfo,
+            isCorrectUserAnswer,
+            userSelectedAnswer: answerIndex,
+          };
+          set({ questions: newQuestions });
+        },
+        goNextQuestion: () => {
+          const { currentQuestion, questions } = get();
+          const nextQuestion = currentQuestion + 1;
+
+          if (nextQuestion < questions.length) {
+            set({ currentQuestion: nextQuestion });
+          }
+        },
+        goPreviousQuestion: () => {
+          const { currentQuestion } = get();
+          const previousQuestion = currentQuestion - 1;
+
+          if (previousQuestion >= 0) {
+            set({ currentQuestion: previousQuestion });
+          }
+        },
       };
-      set({ questions: newQuestions });
     },
-    goNextQuestion: () => {
-      const { currentQuestion, questions } = get();
-      const nextQuestion = currentQuestion + 1;
-
-      if (nextQuestion < questions.length) {
-        set({ currentQuestion: nextQuestion });
-      }
-    },
-    goPreviousQuestion: () => {
-      const { currentQuestion } = get();
-      const previousQuestion = currentQuestion - 1;
-
-      if (previousQuestion >= 0) {
-        set({ currentQuestion: previousQuestion });
-      }
-    },
-  };
-});
+    {
+      name: "questions",
+    }
+  )
+);
