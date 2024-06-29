a8167fa6b4d0638b445a25ea2bc0356d1a5d6f83
Hector Clessi <121068920+hectorc2907@users.noreply.github.com>
2024-06-28 23:40:23 -0300

    sacamos la funcion del color del componente principal

--- projects/13-javascript-quiz-con-zustand/src/Game.tsx
+++ projects/13-javascript-quiz-con-zustand/src/Game.tsx
@@ -13,20 +13,21 @@ import SyntaxHighLighter from "react-syntax-highlighter";
 import { gradientDark } from "react-syntax-highlighter/dist/esm/styles/hljs";
 import { type Question as QuestionType } from "./types";
 
+const getBackgroundColor = (info: QuestionType, index: number) => {
+  const { userSelectedAnswer, correctAnswer } = info;
+  if (userSelectedAnswer == null) return "transparent";
+  if (index !== correctAnswer && index !== userSelectedAnswer)
+    return "transparent";
+  if (index === correctAnswer) return "green";
+  if (index === userSelectedAnswer) return "red";
+
+  return "transparent";
+};
+
 const Question = ({ info }: { info: QuestionType }) => {
   const selectAnswer = useQuestionStore((state) => state.selectAnswer);
   const createHandleClick = (answerIndex: number) => () => {
     selectAnswer(info.id, answerIndex);
-  };
-  const getBackgroundColor = (index: number) => {
-    const { userSelectedAnswer, correctAnswer } = info;
-    if (userSelectedAnswer == null) return "transparent";
-    if (index !== correctAnswer && index !== userSelectedAnswer)
-      return "transparent";
-    if (index === correctAnswer) return "green";
-    if (index === userSelectedAnswer) return "red";
-
-    return "transparent";
   };
   return (
     <Card
@@ -43,7 +44,7 @@ const Question = ({ info }: { info: QuestionType }) => {
             <ListItemButton
               disabled={info.userSelectedAnswer != null}
               onClick={createHandleClick(index)}
-              sx={{ backgroundColor: getBackgroundColor(index) }}
+              sx={{ backgroundColor: getBackgroundColor(info, index) }}
             >
               <ListItemText primary={answer} sx={{ textAlign: "center" }} />
             </ListItemButton>
