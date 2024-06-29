fbbaaa91af16dad77346d7e1afc2ce51c3725458
Hector Clessi <121068920+hectorc2907@users.noreply.github.com>
2024-06-28 22:59:23 -0300

    se configura el modo oscuro de la aplicacion y la muestra de la sintaxis del codigo

--- projects/13-javascript-quiz-con-zustand/src/Game.tsx
+++ projects/13-javascript-quiz-con-zustand/src/Game.tsx
@@ -1,11 +1,16 @@
 import { Card, IconButton, Stack, Typography } from "@mui/material";
 import { useQuestionStore } from "./store/questions";
+import SyntaxHighLighter from "react-syntax-highlighter";
+import { gradientDark } from "react-syntax-highlighter/dist/esm/styles/hljs";
 import { type Question as QuestionType } from "./types";
 
 const Question = ({ info }: { info: QuestionType }) => {
   return (
-    <Card variant="outlined">
+    <Card variant="outlined" sx={{ p: 2, textAlign: "left" }}>
       <Typography variant="h5">{info.question}</Typography>
+      <SyntaxHighLighter language="javascript" style={gradientDark}>
+        {info.code}
+      </SyntaxHighLighter>
     </Card>
   );
 };
--- projects/13-javascript-quiz-con-zustand/src/main.tsx
+++ projects/13-javascript-quiz-con-zustand/src/main.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
@@ -8,8 +9,15 @@ import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
+  <ThemeProvider theme={darkTheme}>
+    <CssBaseline />
     <App />
-  </React.StrictMode>
+  </ThemeProvider>
 );
